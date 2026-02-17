0e191f2378d7e490abfff2063c5b3c99b78ce582
Keita1122 <arc.k.agu1122t@gmail.com>
2026-02-17 23:03:14 +0900

    進捗

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.scss";
 import Header from "@/components/common/header/Header";
-import Hero from "@/components/sections/hero/Hero";
+import Hero from "@/components/sections/hero-section/hero/Hero";
+import Service from "@/components/sections/hero-section/service/Service";
+import WhyUs from "@/components/sections/hero-section/why-us/WhyUs";
+import Company from "@/components/sections/hero-section/company/Company";
+import Contact from "@/components/sections/hero-section/contact/Contact";
+import Footer from "@/components/common/footer/Footer";
 
 
 export default function Home() {
@@ -9,6 +13,11 @@ export default function Home() {
     <div className={styles.page}>
       <Header />
       <Hero />
+      <Service />
+      <WhyUs />
+      <Company />
+      <Contact />
+      <Footer />
     </div>
   );
 }
